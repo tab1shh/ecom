c1aa3f6e01e532dfe72f59a034632fcc997c27cf
tab1shh <164711380+tab1shh@users.noreply.github.com>
2024-08-16 18:34:49 -0400

    created a profile page and update profile function

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 from django import forms
 
 
@@ -85,3 +85,20 @@ def category(request, foo):
 def category_list(request):
     categories = Category.objects.all()
     return render(request, "category_list.html", {"categories": categories})
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "User Has Been Updated!")
+            return redirect("home")
+        return render(request, "update_user.html", {"user_form": user_form})
+    else:
+        messages.success(request, "You Must Be In Logged In To Access Page")
+        return redirect("home")
