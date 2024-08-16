f04e45a58a1e69b97fc41e51e77f23ac683038ca
tab1shh <164711380+tab1shh@users.noreply.github.com>
2024-08-16 17:40:17 -0400

    added a remove button

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -56,3 +56,12 @@ class Cart:
         self.session.modified = True
         thing = self.cart
         return thing
+
+    def delete(self, product):
+        product_id = str(product)
+
+        # remove item from cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
