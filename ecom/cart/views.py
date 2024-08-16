3e833d3297d90a7c83962e556d4972897d9944b2
tab1shh <164711380+tab1shh@users.noreply.github.com>
2024-08-15 22:45:58 -0400

    add to cart button works with id's, need to update cart summary

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 
 def cart_summary(request):
@@ -6,7 +9,21 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    # get the cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+
+        # lookup product in database
+        product = get_object_or_404(Product, id=product_id)
+
+        # save to session
+        cart.add(product=product)
+
+        # return response
+        response = JsonResponse({"Product Name: ": product.name})
+        return response
 
 
 def cart_delete(request):
