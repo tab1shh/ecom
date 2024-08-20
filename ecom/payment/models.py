c2be6694952acaee75c4853e076b5c1b1e3aef60
tab1shh <164711380+tab1shh@users.noreply.github.com>
2024-08-20 19:16:36 -0400

    fix shipping model

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 
 class ShippingAddress(models.Model):
@@ -20,6 +21,15 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f"Shipping Address - {str(self.id)}"
+
+
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+
+post_save.connect(create_shipping, sender=User)
 
 
 class Order(models.Model):
