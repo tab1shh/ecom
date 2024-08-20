f2385f7754384faf6d9614ab4053e8a6315adfd1
tab1shh <164711380+tab1shh@users.noreply.github.com>
2024-08-20 19:40:59 -0400

    billing info

--- ecom/payment/forms.py
+++ ecom/payment/forms.py
@@ -75,3 +75,67 @@ class ShippingForm(forms.ModelForm):
         exclude = [
             "user",
         ]
+
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Name On Card"}
+        ),
+        required=True,
+    )
+    card_number = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Card Number"}
+        ),
+        required=True,
+    )
+    card_exp_date = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Expiration Date"}
+        ),
+        required=True,
+    )
+    card_cvv_number = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "CVV Number"}
+        ),
+        required=True,
+    )
+    card_address1 = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Address 1"}
+        ),
+        required=True,
+    )
+    card_address2 = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Address 1"}
+        ),
+        required=True,
+    )
+    card_city = forms.CharField(
+        label="",
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "City"}),
+        required=True,
+    )
+    card_zipcode = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Zipcode"}
+        ),
+        required=True,
+    )
+    card_country = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Country"}
+        ),
+        required=True,
+    )
--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,7 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        # get the products, quantities, and total from the cart
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # check if user is logged in
+        if request.user.is_authenticated:
+            billing_form = PaymentForm
+            return render(
+                request,
+                "payment/billing_info.html",
+                {
+                    "cart_products": cart_products,
+                    "quantities": quantities,
+                    "totals": totals,
+                    "shipping_info": request.POST,
+                    "billing_form": billing_form,
+                },
+            )
+        else:
+            billing_form = PaymentForm
+            return render(
+                request,
+                "payment/billing_info.html",
+                {
+                    "cart_products": cart_products,
+                    "quantities": quantities,
+                    "totals": totals,
+                    "shipping_info": request.POST,
+                    "billing_form": billing_form,
+                },
+            )
+            pass
+
+        shipping_form = request.POST
+
+        return render(
+            request,
+            "payment/billing_info.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+    else:
+        messages.success(request, "Access Denied")
+        return redirect("home")
 
 
 def payment_success(request):
