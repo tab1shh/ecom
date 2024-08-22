6b0ebe89015d2e80d814af6c411af008149d5a8b
tab1shh <164711380+tab1shh@users.noreply.github.com>
2024-08-22 01:07:19 -0400

    added DBPASS to settings

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "PASSWORD": os.environ["DB_PASSWORD"],
         "HOST": "autorack.proxy.rlwy.net",
         "PORT": "17426",
     }
